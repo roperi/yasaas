b6c2c8041802564ebdce6e63c93bfc82292f7667
Rodo <9901508+roperi@users.noreply.github.com>
2024-06-11 16:16:49 +0100

    Remove unnecessary custom static folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -160,11 +160,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ADMIN_URL = "admin:index"
 
-# Additional locations of static files
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static", "custom"),
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
